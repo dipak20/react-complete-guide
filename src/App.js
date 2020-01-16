7d0f843d31f8ec0c157c5dd5598e5f315f03124b
unknown <dip205pande@gmail.com>
2020-01-16 10:46:54 +0530

    End of Section: Styling Recat Components  Elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
 
-  state ={
+  state = {
     persons : [
       {id: 'Dipak29', name: "Dipak", age: 29},
       {id: 'Yogesh26', name: "Yogesh", age: 26},
@@ -62,6 +62,7 @@ class App extends Component {
   render() {    
 
     let persons= null;
+    let btnClass = '';
 
     if(this.state.showPersons){
       persons= (
@@ -77,27 +78,23 @@ class App extends Component {
         </div> 
       );
 
-/*       style.backgroundColor= 'red';
-      style[':hover']= {
-        backgroundColor: 'salmon',
-        color: 'black'
-      } */       
+      btnClass = classes.Red;
 
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red');    // classes = ['red']
+      assignedClasses.push(classes.red);    // classes = ['red']
     }
     if(this.state.persons.length <= 1){
-      classes.push('bold');  // classes = ['red','bold']
+      assignedClasses.push(classes.bold);  // classes = ['red','bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi I'm a React App...!</h1>
-        <p className={classes.join(' ')}>This is working...!</p>
-        <button className="button" onClick={this.togglePersonHandler}>Toggle Persons</button>
+        <p className={assignedClasses.join(' ')}>This is working...!</p>
+        <button className={btnClass} onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
