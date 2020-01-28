5334a3ee25253ecfc407124b995b005fe79e0ca8
unknown <dip205pande@gmail.com>
2020-01-28 10:41:12 +0530

    After 97th video completion

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+
+// Below useEffect() is executed when the 'props' of 'persons' changes  
+
+/*     useEffect(() => {
+      console.log('[Cockpit.js] useEffect');      
+      //Http request can also be declared in it
+
+      setTimeout(() => {
+        alert('Saved data to cloud...!');
+      },1000);
+    },[props.persons]); */
+
+    useEffect(() => {
+      console.log('[Cockpit.js] useEffect');      
+      //Http request can also be declared in it
+
+      setTimeout(() => {
+        alert('Saved data to cloud...!');
+      },1000);
+
+      return () => {
+        console.log('[Cockpit.js] cleanup work in useEffect');        
+      };
+    },[]);    
+
+    useEffect(() => {
+      console.log('[Cockpit.js] 2nd useEffect');
+      return () => {
+        console.log('[Cockpit.js] cleanup work in 2nd useEffect');        
+      };
+    });
 
     const assignedClasses = [];
     let btnClass = '';
--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends Component {
       {id: 'Yogesh26', name: "Yogesh", age: 26},
       {id: 'Anil28', name: "Anil", age: 28}    
     ],
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   };
 
   static getDerivedStateFromProps(props,state){
@@ -26,6 +27,15 @@ class App extends Component {
 
   componentDidMount(){
     console.log('[App.js] componentDidMount');    
+  }
+
+  shouldComponentUpdate(nextProps,nextState){
+    console.log('[App.js] shouldComponentUpdate');    
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');    
   }
 
 /*   switchNameHandler = (newName) => {
@@ -76,7 +86,6 @@ class App extends Component {
   render() {    
     console.log('[App.js] render');
         
-
     let persons= null;
 
     if(this.state.showPersons){
@@ -89,11 +98,17 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit 
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonHandler} />
+        <button
+          onClick={() => {this.setState({showCockpit: false})}}>
+            Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit  
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonHandler} />
+        ) : null }
         {persons}
       </div>
     );
